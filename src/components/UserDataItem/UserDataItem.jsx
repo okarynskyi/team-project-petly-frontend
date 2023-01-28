0cc581a5a04d90cb7c81b1d3912c7ee84a9849a3
Alena Busova <96522313+al-busova@users.noreply.github.com>
2023-01-29 01:12:43 +0200

    style userData and Item

--- src/components/UserDataItem/UserDataItem.jsx
+++ src/components/UserDataItem/UserDataItem.jsx
@@ -1,12 +1,32 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { LabelUserInfo } from './UserDataItem.styled';
+import { HiPencil, HiCheck } from 'react-icons/hi';
+import { LabelUserInfo, ValueUserInfo, Circle } from './UserDataItem.styled';
 
 export const UserDataItem = ({ type, label, value }) => {
+  const [disable, setDisable] = useState(true);
+  const [startUpdate, setStartUpdate] = useState(false);
   return (
     <>
-      <LabelUserInfo htmlFor={label}>{label}</LabelUserInfo>
-      <input type={type} name={label} id={label} value={value} />
+      <LabelUserInfo htmlFor={label}>{label}:</LabelUserInfo>
+      <div>
+        <ValueUserInfo
+          type={type}
+          name={label}
+          id={label}
+          value={value}
+          disabled={disable}
+        />
+        {startUpdate ? (
+          <Circle type="button">
+            <HiCheck color="#F59256" size="16px" />
+          </Circle>
+        ) : (
+          <Circle type="button">
+            <HiPencil color="#F59256" size="15px" />
+          </Circle>
+        )}
+      </div>
     </>
   );
 };
