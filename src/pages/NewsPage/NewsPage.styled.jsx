983876d8c9d756a35a64e5959b2c84c1711f29b5
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-29 22:42:52 +0200

    стили медиа запросы

--- src/pages/NewsPage/NewsPage.styled.jsx
+++ src/pages/NewsPage/NewsPage.styled.jsx
@@ -1,27 +1,51 @@
 import styled from "styled-components";
+import { device } from 'Device';
 
 export const Section = styled.section`
   background-color: ${p => p.theme.colors.background};
   width: 100%;
   height: 100%;
   padding: 42px 0 100px;
-  @media screen and (min-width: 768px) {
+  @media ${device.tablet} {
     padding: 88px 0 100px;
   }
-  @media screen and (min-width: 1280px) {
+  @media ${device.desktop} {
     padding: 60px 0 100px;
   }
 `;
 
+export const StyledContainer = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 20px;
+
+  @media ${device.mobile} {
+    width: 490px;
+  }
+  @media ${device.tablet} {
+    width: 768px;
+    padding: 0 32px;
+  }
+  @media ${device.desktop} {
+    width: 1280px;
+    padding: 0 16px;
+  }
+`;
+
+
 export const NewsH1 = styled.h1`
-font-family: 'Manrope';
-font-weight: 700;
-font-size: ${props => props.theme.fontSizes.fs48};
+font-family: ${p => p.theme.fonts.main};
+font-weight: ${p => p.theme.fontWeights.fw700};
+font-size: ${p => p.theme.fontSizes.fs24};
 line-height: 1.37;
-color: #111111;
+color: ${p => p.theme.colors.mainText};
 margin-top: 69px;
 margin-bottom: 40px;
 text-align: center;
+
+@media ${device.tablet} {
+  font-size: ${p => p.theme.fontSizes.fs48}; 
+  }
 `;
 
 export const NewsForm = styled.form`
@@ -29,14 +53,20 @@ margin-bottom: 60px;
 text-align: center;
 `;
 export const NewsInput = styled.input`
-width: 608px;
-height: 44px;
 left: 336px;
 top: 232px;
-background: #FFFFFF;
+background-color: ${p => p.theme.colors.white};
 box-shadow: 7px 4px 14px rgba(49, 21, 4, 0.07);
 border-radius: 20px;
 border: none;
+@media ${device.mobile} {
+  width: 280px;
+  height: 40px;
+  }
+@media ${device.tablet} {
+  width: 608px;
+  height: 44px;
+  }
 `;
 
 export const NewsListBox = styled.ul`
@@ -45,37 +75,53 @@ export const NewsListBox = styled.ul`
   flex-wrap: wrap;
 `;
 
-export const NewsListItem = styled.li`
-width: 395px;
-height: 254px;
-padding-left: 16px;
-`;
-
 export const Line = styled.div`
-width: 340px;
-height: 8px;
-left: 16px;
-top: 336px;
+width: 200px;
+height: 4px;
 background: linear-gradient(90deg, #FF634E 0%, #FFDF48 105.44%);
 border-radius: 40px;
+
+@media ${device.tablet} {
+  width: 280px;
+  height: 8px;
+  }
+@media ${device.desktop}{
+  width: 340px;
+  height: 8px;
+  }
+`;
+
+export const NewsDiv = styled.div`
+width: 280px;
+height: 300px;
+
+@media ${device.tablet} {
+  width: 336px;
+  height: 276px;
+  }
+@media ${device.desktop}{
+  width: 395px;
+  height: 254px;
+  }
+
 `;
 
 export const NewsH2 = styled.h2`
-font-family: 'Manrope';
-font-weight: 700;
-font-size: ${props => props.theme.fontSizes.fs24};
+font-family: ${p => p.theme.fonts.main};
+font-weight: ${p => p.theme.fontWeights.fw700};
+font-size: ${p => p.theme.fontSizes.fs24};
 line-height: 1.37;
-color: #111111;
+color: ${p => p.theme.colors.mainText};
 margin-top: 4px;
 margin-bottom: 16px;
 `;
 
 export const NewsListText = styled.p`
-font-family: 'Manrope';
-font-weight: 400;
-font-size: ${props => props.theme.fontSizes.fs16};
+font-family: ${p => p.theme.fonts.main};
+font-weight: ${p => p.theme.fontWeights.fw400};
+font-size: ${p => p.theme.fontSizes.fs16};
 line-height: 1.37;
-color: #111321;
+color: ${p => p.theme.colors.newsText};
 margin-bottom: 40px;
 `;
 
@@ -90,11 +136,10 @@ height: 22px;
 `;
 
 export const A = styled.a`
-font-family: 'Manrope';
+font-family: ${p => p.theme.fonts.main};
 font-style: normal;
-font-weight: 500;
-font-size: ${props => props.theme.fontSizes.fs16};
+font-weight: ${p => p.theme.fontWeights.fw500};
+font-size: ${p => p.theme.fontSizes.fs16};
 line-height: 1.37;
-color: #F59256;
+color: ${p => p.theme.colors.accent};
 `;
-
