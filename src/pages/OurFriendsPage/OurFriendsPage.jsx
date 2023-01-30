46bc5088d9ceb3cf4925addc1606beec56b2807e
fullstack2002 <98652392+fullstack2002@users.noreply.github.com>
2023-01-30 22:42:16 +0200

    linked to backend

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -1,51 +1,21 @@
 import { useEffect, useState } from 'react';
 import Friends from 'components/Friends/Friends';
-// import { getFriends } from 'services/apiService';
+import { getFriends } from 'services/apiService';
 // import Loader from '../../components/Loader';
 import { Section, StyledTitle, StyledContainer } from './OurFriendsPage.styled';
 
 export function OurFriendsPage() {
   const [friends, setFriends] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   const fetchFriends = async () => {
-  //     setIsLoading(true);
-  //     setError(null);
-  //     try {
-  //       const data = await getFriends('friends');
-  //       setFriends(data);
-  //     } catch (e) {
-  //       setError(e);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   };
-  //   fetchFriends();
-  // }, []);
-
-  // useEffect(() => {
-  //   const fetchFriends = async () => {
-  //     const data = await fetch(getFriends);
-  //     console.log(data);
-  //     setFriends(data);
-  //   };
-  //   fetchFriends();
-  // }, []);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-      const fetchFriends = async () => {
-         const response = await fetch(
-            'https://jsonplaceholder.typicode.com/posts?_limit=10'
-         );
-         const data = await response.json();
-         console.log(data);
-         setFriends(data);
-      };
-      fetchFriends();
-   }, []);
-
+    async function fetchData() { 
+      const {data: {friends}} = await getFriends();
+      setFriends([...friends]);
+    }
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -53,7 +23,7 @@ export function OurFriendsPage() {
         <StyledContainer>
           <StyledTitle>Our friends</StyledTitle>
           {/* {isLoading && <Loader />} */}
-          {/* {error && <div>{error.message}</div>} */}
+          {error && <div>{error.message}</div>}
           {friends && <Friends friends={friends} />}
         </StyledContainer>
       </Section>
