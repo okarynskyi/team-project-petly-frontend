ef84fef8e75306076fe1c67aeb1dd189607068eb
Alena Busova <96522313+al-busova@users.noreply.github.com>
2023-02-01 01:59:01 +0200

    valid and style Login

--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -1,60 +1,66 @@
 import styled from 'styled-components';
 import { Form, Field, ErrorMessage } from 'formik';
+import { device } from 'Device';
 
 export const FormFormik = styled(Form)`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  gap: ${props => props.theme.space[3]}px;
-  width: 300px;
+  width: 100%;
   margin: 0 auto;
   button {
-    margin-top: ${props => props.theme.space[3]}px;
-    margin-left: auto;
-    margin-right: auto;
+    margin: ${props => props.theme.space[5]}px auto;
+    width: 280px;
+    font-weight: ${props => props.theme.fontWeights.fw500};
+    letter-spacing: 0.04em;
+  }
+  @media ${device.tablet} {
+    > button {
+      width: 458px;
+    }
   }
 `;
-export const Label = styled.label`
+export const BoxInput = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-around;
-  color: ${props => props.theme.colors.darkgreen};
-  font-size: ${props => props.theme.fontSizes.m}px;
-  font-weight: 500;
-  span {
-    min-width: 120px;
-    margin-right: ${props => props.theme.space[2]}px;
-  }
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: ${props => props.theme.space[2]}px;
 `;
-export const Input = styled.input`
-  width: 180px;
-  padding: ${props => props.theme.space[2]}px;
-  font-size: ${props => props.theme.fontSizes.m}px;
-  border: ${props => `1px solid ${props.theme.colors.darkgreen}`};
-  border-radius: 4px;
-  :hover,
-  :focus {
-    cursor: pointer;
-    border-color: ${props => props.theme.colors.orange};
-  }
-`;
-
 export const InputFormik = styled(Field)`
-  width: 180px;
-  padding: ${props => props.theme.space[2]}px;
-  font-size: ${props => props.theme.fontSizes.m}px;
-  border: ${props => `1px solid ${props.theme.colors.darkgreen}`};
-  border-radius: 4px;
+  width: 280px;
+  padding: 11px 0 11px 14px;
+  font-family: ${props => props.theme.fonts.main};
+  font-size: ${props => props.theme.fontSizes.fs14};
+  line-height: 1.35;
+  letter-spacing: 0.04em;
+  background: ${props => props.theme.colors.background};
+  border: ${props => props.theme.borders.auth};
+  border-radius: ${props => props.theme.radii.br40};
   :hover,
   :focus {
     cursor: pointer;
-    border-color: ${props => props.theme.colors.orange};
+    border-color: ${props => props.theme.colors.accent};
+  }
+  @media ${device.tablet} {
+    width: 448px;
+    font-size: ${props => props.theme.fontSizes.fs18};
+  }
+  @media ${device.desktop} {
+    width: 458px;
   }
 `;
-
 export const ErrorText = styled(ErrorMessage)`
   width: 100%;
-  margin: 0 ;
-  font-size: ${props => props.theme.fontSizes.s}px;
-  color: ${props => props.theme.colors.orange};
+  text-align: center;
+  font-size: ${props => props.theme.fontSizes.fs14};
+  color: ${props => props.theme.colors.accent};
+`;
+export const Question = styled.p`
+  font-family: ${props => props.theme.fonts.main};
+  font-size: ${props => props.theme.fontSizes.fs12};
+  text-align: center;
+  letter-spacing: 0.04em;
+  color: ${props => props.theme.colors.inputText};
+  > a {
+    text-decoration-line: underline;
+    color: #3091eb;
+  }
 `;
