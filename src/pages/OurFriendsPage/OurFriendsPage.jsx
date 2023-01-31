6833b7ee97dd47952b8af29e7e2ff5fcc3356dd3
fullstack2002 <98652392+fullstack2002@users.noreply.github.com>
2023-01-31 12:19:40 +0200

    bugfix friends

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react';
 import Friends from 'components/Friends/Friends';
 import { getFriends } from 'services/apiService';
-// import Loader from '../../components/Loader';
 import { Section, StyledTitle, StyledContainer } from './OurFriendsPage.styled';
 
 export function OurFriendsPage() {
   const [friends, setFriends] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchData() { 
@@ -22,8 +19,6 @@ export function OurFriendsPage() {
       <Section>
         <StyledContainer>
           <StyledTitle>Our friends</StyledTitle>
-          {/* {isLoading && <Loader />} */}
-          {error && <div>{error.message}</div>}
           {friends && <Friends friends={friends} />}
         </StyledContainer>
       </Section>
