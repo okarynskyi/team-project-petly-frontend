71641e75f2ceaa4cbc90ab48965f775511b71128
Ivan <rosolovvlad@gmail.com>
2023-02-01 00:13:27 +0200

    Spinner

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,1 +1,27 @@
+import React from 'react';
+import { MutatingDots } from 'react-loader-spinner';
 
+export const Loader = () => {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+      <MutatingDots
+        height="100"
+        width="100"
+        color="#f59256"
+        secondaryColor="#f59256"
+        radius="12.5"
+        ariaLabel="mutating-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+};
