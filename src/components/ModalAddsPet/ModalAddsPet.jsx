db2f17409ef9bb020019ba7efdbfd7603253e07a
Alena Busova <96522313+al-busova@users.noreply.github.com>
2023-02-02 17:52:57 +0200

    add modal delete, start modalAdd

--- src/components/ModalAddsPet/ModalAddsPet.jsx
+++ src/components/ModalAddsPet/ModalAddsPet.jsx
@@ -1,21 +1,43 @@
-import Modal from '../common/Modal/Modal'
-import { Card } from './ModalAddsPet.styled';
+import {
+  WrapperAddPet,
+  StyledPlus,
+  AddPetBtn,
+  WrapperModalAddPet,
+} from './ModalAddsPet.styled';
+import { useState } from 'react';
+import Modal from '../common/Modal/Modal';
 
-const ModalAddPet = ({setActive, active}) => {
+const ModalAddPet = () => {
+  const [modalActive, setModalActive] = useState(false);
   return (
     <main>
-      <Modal 
-      active={active} 
-      setActive={setActive}
-      >
-        <Card>
-        <>
-       text
-        </>
-        </Card>
+      <WrapperAddPet>
+        <p>Add pet </p>
+        <AddPetBtn onClick={() => setModalActive(true)}>
+          <StyledPlus />
+        </AddPetBtn>
+      </WrapperAddPet>
+      <Modal active={modalActive} setActive={setModalActive}>
+        <WrapperModalAddPet>
+          <h3>Add pet</h3> 
+          <form >
+            <label htmlFor="">
+              Name pet
+              <input type="text" />
+            </label>
+            <label htmlFor="">
+              Date of birth
+              <input type="text" />
+            </label>
+            <label htmlFor="">
+              Breed
+              <input type="text" />
+</label>
+
+          </form>
+        </WrapperModalAddPet>
       </Modal>
     </main>
   );
 };
-
 export default ModalAddPet;
