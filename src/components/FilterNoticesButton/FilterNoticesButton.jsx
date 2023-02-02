2f4c46723bda3477703a56badbfd2981d8a14b9c
RomanUstychenko <roman.ustychenko@gmail.com>
2023-02-02 16:03:19 +0200

    Update FilterNoticesButton.jsx

--- src/components/FilterNoticesButton/FilterNoticesButton.jsx
+++ src/components/FilterNoticesButton/FilterNoticesButton.jsx
@@ -3,14 +3,13 @@ import { useSelector } from 'react-redux';
 import { selectToken } from 'redux/auth/authSelectors';
 
 import { nanoid } from 'nanoid';
-// import { v4 as uuidv4 } from 'uuid';
 
 import { Button, FilterList, Item, Wrapper } from './FilterNoticesButton.styled.jsx';
-import { useLocation } from 'react-router-dom';
 import { AddNoticeButton } from '../../components/AddNoticeButton/AddNoticeButton';
 
 import { useState } from 'react';
-import ModalAddPet from 'components/ModalAddsPet/ModalAddsPet.jsx';
+// import ModalAddPet from 'components/ModalAddsPet/ModalAddsPet.jsx';
+import ModalAddNotice from 'components/ModalAddNotice/ModalAddNotice.jsx';
 
 
 const buttons = [
@@ -41,26 +40,18 @@ const authButtons = [
 
 function FilterNoticesButton() {
   const token = useSelector(selectToken);
-// const unauthorizedFilterId = nanoid();
-// const authorizedFilterId = nanoid();
-  const location = useLocation();
-  const category = location.pathname.split('/')[2];
-  // console.log(category)
-
   const [modalActive, setModalActive] = useState(false);
 
   return (
     <Wrapper>
       <FilterList>
-        {/* {console.log(test)} */}
         {buttons.map(b => (
           <Item key={nanoid()}>
             <Button
-              to={b.link === category ? '/notices/' : b.link}
+              to={'/notices/' + b.link}
               name={b.link}
             >
               {b.btn}
-              {/* {console.log(b.link)} */}
             </Button>
           </Item>
         ))}
@@ -68,7 +59,7 @@ function FilterNoticesButton() {
           authButtons.map(b => (
             <Item key={nanoid()}>
               <Button
-                to={b.link === category ? '/notices/' : b.link}
+                to={'/notices/' + b.link}
                 name={b.link}
               >
                 {b.btn}
@@ -80,11 +71,11 @@ function FilterNoticesButton() {
       onClick={() => setModalActive(true)}
       >
       </AddNoticeButton>
-      <ModalAddPet
+      <ModalAddNotice
       active={modalActive} 
       setActive={setModalActive}
       >
-        </ModalAddPet>
+        </ModalAddNotice>
     </Wrapper>
   );
 }
