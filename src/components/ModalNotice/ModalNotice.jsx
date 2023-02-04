fb5e6517e5a1b7eb6c0d569596f3d4d48ce5db15
fullstack2002 <98652392+fullstack2002@users.noreply.github.com>
2023-02-04 21:48:56 +0200

    modalnotice

--- src/components/ModalNotice/ModalNotice.jsx
+++ src/components/ModalNotice/ModalNotice.jsx
@@ -1,19 +1,67 @@
-// import { useState } from 'react';
-import Modal from '../common/Modal/Modal'
+import Modal from '../common/Modal/Modal';
+// import operations from '../../redux/notices/noticesOperations';
+// import { addToFavorites, deleteFromFavorites } from '../../redux/notices/noticesOperations';
+// import { useDispatch } from 'react-redux';
+import { Button } from './ModalNotice.styled';
+import { useState } from 'react';
+// import { toast } from 'react-toastify';
 
-const ModalNotice = ({setActive, active}) => {
+const ModalNotice = ({ notice = {}, closeModal, getBtnInfo, token, path }) => {
+  const [modalActive, setModalActive] = useState(false);
+  // const noItem = '-------------';
+  // const noPrice = '0';
+
+  //  const {
+  //   _id,
+  //   avatarURL,
+  //   favorite,
+  //   title,
+  //   category,
+  //   name,
+  //   birthday,
+  //   breed,
+  //   location,
+  //   sex,
+  //   owner,
+  //   comments,
+  //   price,
+  //   own,
+  // } = notice;
+  // const dispatch = useDispatch();
+
+  // const delNotice = id => {
+  //   dispatch(deleteUserNotice(id));
+  //   closeModal();
+  // };
+
+  // const handleFavorite = async id => {
+  //   if (!Boolean(token)) {
+  //     toast.warn('You are not a registered user!');
+  //     return;
+  //   }
+  //   if (favorite) {
+  //     const { payload } = await dispatch(deleteFromFavorites(id));
+  //     if (payload === id && path === 'favorite') {
+  //       await dispatch(deleteFromFavorites(id));
+  //       closeModal();
+  //     }
+  //   } else {
+  //     dispatch(addToFavorites(id));
+  //   }
+  //   return;
+  // };
+
   return (
-    <main>
-      <Modal 
-      active={active} 
-      setActive={setActive}
-      >
-        <>
-       text
-        </>
-      
-      </Modal>
-    </main>
+    <>
+      <Button onClick={() => setModalActive(true)}>Learn more</Button>
+       <Modal 
+      active={modalActive} 
+      setActive={setModalActive}>
+          <>
+          text
+          </>
+       </Modal>
+    </>
   );
 };
 
