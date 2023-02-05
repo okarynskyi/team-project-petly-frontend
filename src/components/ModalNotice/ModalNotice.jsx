72cc6679fbda64a29967430e6580218c8e53fa74
fullstack2002 <98652392+fullstack2002@users.noreply.github.com>
2023-02-05 17:36:12 +0200

    modalnotice styles
    not done yet!

--- src/components/ModalNotice/ModalNotice.jsx
+++ src/components/ModalNotice/ModalNotice.jsx
@@ -2,9 +2,10 @@ import Modal from '../common/Modal/Modal';
 // import operations from '../../redux/notices/noticesOperations';
 // import { addToFavorites, deleteFromFavorites } from '../../redux/notices/noticesOperations';
 // import { useDispatch } from 'react-redux';
-import { Button } from './ModalNotice.styled';
+import { Button, ImgModal, FilterTitleBox, FitlerTitle, ModalTitle, ModalList, ModalListItem } from './ModalNotice.styled';
 import { useState } from 'react';
 // import { toast } from 'react-toastify';
+import { ModalBox } from './ModalNotice.styled';
 
 const ModalNotice = ({ notice = {}, closeModal, getBtnInfo, token, path }) => {
   const [modalActive, setModalActive] = useState(false);
@@ -54,12 +55,50 @@ const ModalNotice = ({ notice = {}, closeModal, getBtnInfo, token, path }) => {
   return (
     <>
       <Button onClick={() => setModalActive(true)}>Learn more</Button>
-       <Modal 
+        <Modal 
       active={modalActive} 
       setActive={setModalActive}>
-          <>
-          text
-          </>
+        <ModalBox>
+          <div position="relative">
+            <ImgModal alt="" width="50" height="50" />
+            <FilterTitleBox>
+              <FitlerTitle>for sale</FitlerTitle>
+            </FilterTitleBox>
+          </div>
+          <div>
+            <ModalTitle>Cute dog looking for a home</ModalTitle>
+            <ModalList>
+              <ModalListItem>
+                <p>Name:</p>
+                <p>Clyde</p>
+              </ModalListItem>
+              <ModalListItem>
+                <p>Birthday:</p>
+                <p>12.10.2019</p>
+              </ModalListItem>
+              <ModalListItem>
+                <p>Breed:</p>
+                <p>Schnauzer mix</p>
+              </ModalListItem>
+              <ModalListItem>
+                <p>Location:</p>
+                <p>LA</p>
+              </ModalListItem>
+              <ModalListItem>
+                <p>The sex:</p>
+                <p>male</p>
+              </ModalListItem>
+              <ModalListItem>
+                <p>Email:</p>
+                <p>xxx@gmail.com</p>
+              </ModalListItem>
+              <ModalListItem>
+                <p>Phone:</p>
+                <p>+380971234567</p>
+              </ModalListItem>
+            </ModalList>
+          </div>
+          </ModalBox>
        </Modal>
     </>
   );
--- src/components/ModalNotice/ModalNotice.styled.jsx
+++ src/components/ModalNotice/ModalNotice.styled.jsx
@@ -30,3 +30,78 @@ export const Button = styled.button`
     margin-left: 20px;
   }
 `;
+
+export const ModalBox = styled.div`
+  border: 3px solid blue;
+  width: 280px;
+  height: 530px;
+  padding: 40px 20px;
+  display: flex;
+  align-items: center;
+
+  @media ${device.tablet} {
+    width: 704px;
+}
+
+  // @media ${device.desktop}{
+      
+  // }
+`;
+
+export const ImgModal = styled.img`
+  border: 3px solid green;
+  width: 240px;
+  height: 240px;
+  border-radius: 0px 0px 40px 40px;
+  background: ${p => p.theme.colors.accent};
+
+  @media ${device.tablet} {
+    width: 288px;
+    height: 328px;
+  }
+  `
+
+export const FilterTitleBox = styled.div`
+  width: 158px;
+  height: 28px;
+  position: absolute;
+  border-radius: 0px 40px 40px  0px;
+  background: #ffffff99;
+  backdrop-filter: blur(2px);
+  border: 3px solid red;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 141px;
+  left: 43px;
+`
+
+export const FitlerTitle = styled.p`
+  font-family: ${p => p.theme.fonts.main};
+  font-weight: ${p => p.theme.fontWeights.fw500};
+  font-size: ${p => p.theme.fontSizes.fs12};
+  line-height: 16px;
+  align-items: center;
+  color: ${p => p.theme.colors.mainText};
+`
+
+export const ModalTitle = styled.h2`
+  border: 3px solid cyan;
+  font-family: ${p => p.theme.fonts.main};
+  font-weight: ${p => p.theme.fontWeights.fw700};
+  font-size: ${p => p.theme.fontSizes.fs24};
+  line-height: 33px;
+  color: ${p => p.theme.colors.black};
+
+  @media ${device.tablet} {
+    font-size: ${p => p.theme.fontSizes.fs28};
+    line-height: 38px;
+  }
+`
+export const ModalList = styled.ul`
+  border: 3px solid maroon;
+`
+export const ModalListItem = styled.li`
+  border: 3px solid limegreen;
+`
