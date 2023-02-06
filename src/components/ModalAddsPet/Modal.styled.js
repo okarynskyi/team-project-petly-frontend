08e67c818898f22386d859dc94b927d691e12c13
Alena Busova <96522313+al-busova@users.noreply.github.com>
2023-02-07 00:53:57 +0200

    fix update and modal

--- src/components/ModalAddsPet/Modal.styled.js
+++ src/components/ModalAddsPet/Modal.styled.js
@@ -10,25 +10,22 @@ export const ModalBackdrop = styled.div`
   @media screen and (max-width: 767px) {
   }
   @media ${device.tablet} {
- 
   }
 `;
 export const ModalContent = styled.div`
- position: absolute;
+  position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  min-height: 300px;
-  max-width: 600px;
-  width: 100%;
-  padding: 12px;
-  background-color: #fff;
-  border-radius: 3px;
+  height: 530px;
+  max-height: 100%;
+  width: 280px;
+  max-width: 100%;
+  background: #ffffff;
+  border-radius: 20px;
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
-  @media screen and (max-width: 767px) {
-  }
   @media ${device.tablet} {
- 
+    width: 620px;
   }
 `;
